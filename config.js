06218feee8e0b679b14fe2496256c5967b3ae779
TZOUVELI PARASKEVI <39123087+parasktz@users.noreply.github.com>
2026-04-06 12:19:43 +0300

    Update Firebase config with placeholder values
    Replaced Firebase configuration values with placeholders.

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyACxHcH_-kiIfxEtm_L5u2Gd3MhJJsTp7o",
-  authDomain: "cultureboteval.firebaseapp.com",
-  databaseURL: "https://cultureboteval-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "cultureboteval",
-  storageBucket: "cultureboteval.firebasestorage.app",
-  messagingSenderId: "463119543508",
-  appId: "1:463119543508:web:581f7336399af2d7de1f41"
+  apiKey:            "PLACEHOLDER",
+  authDomain:        "PLACEHOLDER",
+  databaseURL:       "PLACEHOLDER",
+  projectId:         "PLACEHOLDER",
+  storageBucket:     "PLACEHOLDER",
+  messagingSenderId: "PLACEHOLDER",
+  appId:             "PLACEHOLDER"
 };
 
 // Initialize Firebase
