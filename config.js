be1a72ddc829e103186459dfadea7050c4bb6851
TZOUVELI PARASKEVI <39123087+parasktz@users.noreply.github.com>
2026-04-06 13:36:33 +0300

    Rename FIREBASE_CONFIG to firebaseConfig

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 // ║  Project Settings →      Your apps → Web app → Config     ║
 // ╚══════════════════════════════════════════════════════╝
 
-const FIREBASE_CONFIG = {
- apiKey: "AIzaSyA_aHBdW2p5BwfZZhwYK99ybWr2aOPS8fs",
+const firebaseConfig = {
+  apiKey: "AIzaSyA_aHBdW2p5BwfZZhwYK99ybWr2aOPS8fs",
   authDomain: "cultureboteval.firebaseapp.com",
   databaseURL: "https://cultureboteval-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "cultureboteval",
